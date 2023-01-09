f2c11560d801eaa1090678bce32115d3e668c206
Cayden Glenn Park <caydenpark@Caydens-MacBook-Pro.local>
2023-01-09 13:32:48 -0700

    Prep 2: wrote program for  grade percentage.

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,42 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("What is your grade percentage? ");
+        string answer = Console.ReadLine();
+        int gradePercentage = int.Parse(answer);
+
+        string letter = "";
+
+        if (gradePercentage >= 90)
+        {
+            letter = "A";
+        }
+        else if (gradePercentage >= 80)
+        {
+            letter = "B";
+        }
+        else if (gradePercentage >= 70)
+        {
+            letter = "C";
+        }
+        else if (gradePercentage >= 60)
+        {
+            letter = "D";
+        }
+        else
+        {
+            letter = "F";
+        }
+
+        Console.WriteLine($"Your letter grade is {letter}");
+
+        if (gradePercentage >= 60)
+        {
+            Console.WriteLine("Congrats, you passed the class!");
+        }
+        else
+        {
+            Console.WriteLine("You failed the class. Better luck next time.");
+        }
     }
 }
