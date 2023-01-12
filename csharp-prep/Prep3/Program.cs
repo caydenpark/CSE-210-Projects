7f4f5b5b1d30ff69382a6cbdff0d837bd419ff28
Cayden Glenn Park <caydenpark@Caydens-MacBook-Pro.local>
2023-01-11 20:23:40 -0700

    prep 3 activity

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,13 +4,15 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.Write("What is the magic number? ");
-        int magicNumber = int.Parse(Console.ReadLine());
-        Console.Write("What is your guess? ");
-        int guess = int.Parse(Console.ReadLine());
+        Random randomGenerator = new Random();
+        int magicNumber = randomGenerator.Next(1,100);
+        int guess = -1;
         
         while(guess != magicNumber)
         {
+            Console.Write("What is your guess? ");
+            guess = int.Parse(Console.ReadLine());
+
             if(guess > magicNumber)
             {
                 Console.WriteLine("Lower");
